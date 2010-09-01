b4f05436b447da0294dfad150b962c595d4c461e
Ben Weller <ben@tapvt.com>
2010-09-01 03:09:03 -0400

    minor display logic revision for last crumb

--- views/breadcrumbs/layout.php
+++ views/breadcrumbs/layout.php
@@ -5,14 +5,17 @@
  * @author Kieran Graham
  */
 ?>
-<? if (count($breadcrumbs) > 0) : ?>
+<? $c = count($breadcrumbs) ?>
+
 <ul id="breadcrumbs">
+<? if ($c > 1) : ?>
 <? foreach ($breadcrumbs as $crumb) : ?>
-<? if ($crumb->get_url() !== NULL) :  ?>
-	<li><a href="<?=$crumb->get_url()?>"><?=$crumb->get_title()?></a></li>
+<? if ($crumb->get_url() !== NULL && $c > 1) :  ?>
+	<li><a href="<?=$crumb->get_url()?>"><?=$crumb->get_title()?></a> <?= ( $c != 1 ? '>' : '' ) ?></li>
 <? else : ?>
 	<li><?=$crumb->get_title()?></li>
 <? endif; ?>
+<? $c-- ?>
 <? endforeach; ?>
-</ul>
 <? endif; ?>
+</ul>
